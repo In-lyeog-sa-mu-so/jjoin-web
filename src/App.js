10f76ea24d857d8489c47c84d0ea274cd4364fa8
jun6292 <c68254@gmail.com>
2023-11-05 19:12:16 +0900

    feat: Topbar, Sidebar 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from "react";
 import './App.css';
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-        <h1>hello world!</h1>
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          {/* <Route exact path="/" element={<Home />} /> */}
+          {/* <Route path="/users" element={<UserList/>} />
+          <Route path="/calendar" element={<ReactCalendar/>} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
