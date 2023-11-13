9b3cc7bdd4402eed7d01ba54cb64bc63f8289a28
taewook <davis0625@dongguk.edu>
2023-11-14 04:48:34 +0900

    FIX: Notice 연동 테스트 및 수정

--- src/pages/Notice/NoticeList/NoticeListPage.jsx
+++ src/pages/Notice/NoticeList/NoticeListPage.jsx
@@ -3,6 +3,7 @@ import {Link, useNavigate, useParams} from "react-router-dom";
 import axios from 'axios';
 import styled from "styled-components";
 import CommonTable from "./CommonTable";
+import { useLocation } from 'react-router-dom';
 
 const Tr = styled.tr`
   &:hover {
@@ -23,15 +24,15 @@ const StyledLink = styled(Link)`
   &:hover {
     font-weight: bold;
     text-decoration: underline;
-}
+  }
 `;
 const Container = styled.div`
-  float: right;  
+  float: right;
   display: flex;
   padding-bottom:10px;
   margin-right:5%;
   & > button {
-    height: 25px; 
+    height: 25px;
     margin-left: 10px;
     cursor: pointer;
   }
@@ -47,17 +48,20 @@ const Container = styled.div`
   justify-content: center;
   margin-top: 5%;
 `;*/
-
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 function NoticeList() {
     const navigate = useNavigate();
     const [noticeList, setNoticeList] = useState([]);
     const baseUrl="https://7f43ee63-b0b8-4e87-9c96-a7c2c01a39f5.mock.pstmn.io";
-    /*const [page, setPage] = useState(0);
-    const size = 10;*/
-    const { clubId } = useParams();
+    const {clubId} = useParams();
+    const query = useQuery();
+    const page = query.get('page');
+    const size = query.get('size');
     const getBoardList = async () => {
         try {
-            const resp = await axios.get(`${baseUrl}/manager/club/${clubId}/notice`);
+            const resp = await axios.get(`${baseUrl}/manager/club/${clubId}/notice?page=${page}&size=${size}`);
             if(resp && resp.data) {
                 setNoticeList(resp.data);
             } else {
@@ -69,7 +73,7 @@ function NoticeList() {
     };
     useEffect(() => {
         getBoardList(); // 1) 게시글 목록 조회 함수 호출
-    }, [clubId]);
+    }, [clubId,page]);
     const moveToWrite = () => {
         navigate(`/manager/club/${clubId}/write`);
     };
@@ -77,15 +81,6 @@ function NoticeList() {
     return (
         <div>
             <H2>공지사항</H2>
-            <Container>
-                <select>
-                    <option value="">-선택-</option>
-                    <option value="title">제목</option>
-                    <option value="contents">내용</option>
-                </select>
-                <input type="text"/>
-                <button>검색</button>
-            </Container>
             <CommonTable headersName={['글번호', '제목', '작성일']}>
                 {noticeList&&noticeList.map((notice)=> (
                     <Tr key={notice.id}>
@@ -95,7 +90,7 @@ function NoticeList() {
                                 {notice.title}
                             </StyledLink>
                         </Td>
-                        <Td>{notice.updateddata}</Td>
+                        <Td>{notice.updatedDate}</Td>
                     </Tr>
                 ))}
             </CommonTable>
--- src/pages/Notice/NoticeUpdatePage.jsx
+++ src/pages/Notice/NoticeUpdatePage.jsx
@@ -63,13 +63,11 @@ const NoticeUpdatePage = () => {
     const navigate = useNavigate();
     const { clubId,id } = useParams();
     const [notice, setNotice] = useState({
-        id: 0,
         title: '',
-        updateddata: '',
-        contents: '',
+        content: ''
     });
 
-    const { title, updateddata, contents } = notice; //비구조화 할당
+    const { title, content } = notice; //비구조화 할당
     const baseUrl="https://7f43ee63-b0b8-4e87-9c96-a7c2c01a39f5.mock.pstmn.io";
     const onChange = (event) => {
         const { value, name } = event.target;
@@ -121,10 +119,10 @@ const NoticeUpdatePage = () => {
                     <CONTENT>
                         <span>내용</span>
                         <textarea
-                            name="contents"
+                            name="content"
                             cols="30"
                             rows="10"
-                            value={contents}
+                            value={content}
                             onChange={onChange}
                         ></textarea>
                     </CONTENT>
