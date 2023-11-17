f9329cfdd7811357e01172cbcdbaa9a3e52fe9bb
jun6292 <c68254@gmail.com>
2023-11-17 14:59:18 +0900

    feat: 인원 관리, 가입 신청 관리(더미 데이터)

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import './userList.css';
 import ApplicationList from './ApplicationList';
 import { DataGrid } from '@mui/x-data-grid';
-import { userRows } from '../../dummyData';
+// import { userRows } from '../../dummyData'
 import { DeleteOutline } from '@mui/icons-material';
+
+const userRows = [
+    { id: 1, name: "조원준", student_id: '2019112060', department: '컴퓨터공학과', email: 'c68254@gmail.com', join_date: '2023-10-06', user_type: '회장'},
+    { id: 2, name: "김태욱", student_id: '2019112061', department: '컴퓨터공학과', email: 'taewook@naver.com', join_date: '2023-10-06', user_type: '대국장'},
+    { id: 3, name: "조성진", student_id: '2019112062', department: '컴퓨터공학과', email: 'csk00@gmail.com-xxxx', join_date: '2023-10-07', user_type: '부회장'},
+    { id: 4, name: "박재형", student_id: '2019112063', department: '컴퓨터공학과', email: 'hyeong@dgu.ac.kr', join_date: '2023-10-07', user_type: '기획국장'},
+    { id: 5, name: "손형준", student_id: '2019112064', department: '컴퓨터공학과', email: 'dcs.public.gmail.com', join_date: '2023-10-08', user_type: '사무국장'},
+    { id: 6, name: "이선호", student_id: '2019112065', department: '컴퓨터공학과', email: 'prefer00@dongguk.edu', join_date: '2023-10-09', user_type: '관리국장'},
+];
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70, },
     { field: 'name', headerName: '이름', width: 150, },
     { field: 'student_id', headerName: '학번', width: 150, },
     { field: 'department', headerName: '학과', width: 150, },
-    { field: 'phone', headerName: '전화번호', width: 150, },
+    { field: 'email', headerName: '이메일', width: 150, },
     { field: 'join_date', headerName: '가입일', width: 150, },
     { field: 'user_type', headerName: '직책', width: 150, },
     { 
@@ -26,12 +35,14 @@ const columns = [
             );
         },
     },
-]
+];
 
 export default function UserList() {
     return (
         <div className="userList">
             <h3>멤버관리</h3>
+            <hr color="darkBlue"/>
+            <br />
             <DataGrid
                 rows={userRows}
                 disableRowSelectionOnClick
@@ -39,8 +50,8 @@ export default function UserList() {
                 pageSizeOptions={5}
                 rowsPerPageOptions={[5]}
                 checkboxSelection
-                autoPageSize
             />
+            <br /><br />
             <ApplicationList />
         </div>
     );
